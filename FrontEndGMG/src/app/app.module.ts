ea677f1b2053b212f947e92f3bd0c5dd0d8fe8be
Gabriela <95320907+Gabita2020@users.noreply.github.com>
2022-10-17 18:29:41 -0300

    cambios

--- FrontEndGMG/src/app/app.module.ts
+++ FrontEndGMG/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExpeienciaComponent } from './components/expeiencia/expeiencia.component';
+import { ExpeienciaComponent } from './components/experiencia/expeiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { HysComponent } from './components/hys/hys.component';
 import { CircleProgressComponent, NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -39,6 +41,12 @@ import { FooterComponent } from './components/footer/footer.component';
     AppRoutingModule,
     FormsModule,
     NgCircleProgressModule.forRoot({}),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
 
   ],
   providers: [],
